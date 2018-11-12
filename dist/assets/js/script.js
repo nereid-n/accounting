053310aa203def351ed93f40b3898ce128a0db7e
nereid <nas.kuleshova@gmail.com>
2018-11-12 15:06:17 +0300

    end index

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -53,4 +53,19 @@ if ($('.partners__slider').length > 0) {
     }]
   });
 }
+
+if ($('.reviews__img-slider').length > 0) {
+  $('.reviews__img-slider').slick({
+    asNavFor: '.reviews__text-slider',
+    prevArrow: '<button class="btn-slider reviews__prev"><img src="assets/images/icons/icon-arrow-left-white.png" alt=""></button>',
+    nextArrow: '<button class="btn-slider btn-slider-black reviews__next"><img src="assets/images/icons/icon-arrow-right-dark.png" alt=""></button>'
+  });
+}
+
+if ($('.reviews__text-slider').length > 0) {
+  $('.reviews__text-slider').slick({
+    asNavFor: '.reviews__img-slider',
+    arrows: false
+  });
+}
 //# sourceMappingURL=script.js.map
---
+++ src/components/index/reviews/reviews.js
@@ -0,0 +1,14 @@
+if ($('.reviews__img-slider').length > 0) {
+  $('.reviews__img-slider').slick({
+    asNavFor: '.reviews__text-slider',
+    prevArrow: '<button class="btn-slider reviews__prev"><img src="assets/images/icons/icon-arrow-left-white.png" alt=""></button>',
+    nextArrow: '<button class="btn-slider btn-slider-black reviews__next"><img src="assets/images/icons/icon-arrow-right-dark.png" alt=""></button>',
+  });
+}
+
+if ($('.reviews__text-slider').length > 0) {
+  $('.reviews__text-slider').slick({
+    asNavFor: '.reviews__img-slider',
+    arrows: false
+  });
+}
